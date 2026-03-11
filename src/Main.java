1ac70a17e1b3eff2d24035de64c65e909496c371
GargiSinghh <gs6295@srmist.edu.in>
2026-03-11 15:12:00 +0530

    UC8:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,12 @@
 // (powered by Fernflower decompiler)
 //
 
-import java.util.ArrayDeque;
+class UseCase8PalindromeCheckerApp$Node {
+    char data;
+    UseCase8PalindromeCheckerApp$Node next;
 
-public class UseCase7PalindromeCheckerApp {
-    public static void main(String[] var0) {
-        String var1 = "refer";
-        ArrayDeque var2 = new ArrayDeque();
-
-        for(char var6 : var1.toCharArray()) {
-            var2.addLast(var6);
-        }
-
-        boolean var7 = true;
-
-        while(var2.size() > 1) {
-            char var8 = (Character)var2.removeFirst();
-            char var9 = (Character)var2.removeLast();
-            if (var8 != var9) {
-                var7 = false;
-                break;
-            }
-        }
-
-        System.out.println("Input : " + var1);
-        System.out.println("Is Palindrome? : " + var7);
+    UseCase8PalindromeCheckerApp$Node(char var1) {
+        this.data = var1;
+        this.next = null;
     }
 }
