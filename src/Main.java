dd82fe5902e89d5a56ef736722308020b7c48503
GargiSinghh <gs6295@srmist.edu.in>
2026-03-11 15:10:46 +0530

    UC7:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,29 @@
 // (powered by Fernflower decompiler)
 //
 
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.ArrayDeque;
 
-public class UseCase6PalindromeCheckerApp {
+public class UseCase7PalindromeCheckerApp {
     public static void main(String[] var0) {
-        String var1 = "civic";
-        LinkedList var2 = new LinkedList();
-        Stack var3 = new Stack();
+        String var1 = "refer";
+        ArrayDeque var2 = new ArrayDeque();
 
-        for(char var7 : var1.toCharArray()) {
-            var2.add(var7);
-            var3.push(var7);
+        for(char var6 : var1.toCharArray()) {
+            var2.addLast(var6);
         }
 
-        boolean var8 = true;
+        boolean var7 = true;
 
-        while(!var2.isEmpty()) {
-            if (var2.remove() != var3.pop()) {
-                var8 = false;
+        while(var2.size() > 1) {
+            char var8 = (Character)var2.removeFirst();
+            char var9 = (Character)var2.removeLast();
+            if (var8 != var9) {
+                var7 = false;
                 break;
             }
         }
 
         System.out.println("Input : " + var1);
-        System.out.println("Is Palindrome? : " + var8);
+        System.out.println("Is Palindrome? : " + var7);
     }
 }
