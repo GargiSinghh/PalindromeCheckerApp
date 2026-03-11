1c8f800709d1c9fff918a49b140986c57776b668
GargiSinghh <gs6295@srmist.edu.in>
2026-03-11 15:07:25 +0530

    UC5:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,27 @@
 // (powered by Fernflower decompiler)
 //
 
-public class UseCase4PalindromeCheckerApp {
-    public static void main(String[] var0) {
-        String var1 = "radar";
-        char[] var2 = var1.toCharArray();
-        int var3 = 0;
-        int var4 = var2.length - 1;
+import java.util.Stack;
 
-        boolean var5;
-        for(var5 = true; var3 < var4; --var4) {
-            if (var2[var3] != var2[var4]) {
-                var5 = false;
+public class UseCase5PalindromeCheckerApp {
+    public static void main(String[] var0) {
+        String var1 = "noon";
+        Stack var2 = new Stack();
+
+        for(char var6 : var1.toCharArray()) {
+            var2.push(var6);
+        }
+
+        boolean var8 = true;
+
+        for(char var7 : var1.toCharArray()) {
+            if (var7 != (Character)var2.pop()) {
+                var8 = false;
                 break;
             }
-
-            ++var3;
         }
 
         System.out.println("Input : " + var1);
-        System.out.println("Is Palindrome? : " + var5);
+        System.out.println("Is Palindrome? : " + var8);
     }
 }
