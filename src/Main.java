22551fd2594dd7589ef87e164f305c6234120175
GargiSinghh <gs6295@srmist.edu.in>
2026-03-11 15:14:33 +0530

    UC9:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,50 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
+/**
+ * =========================================================
+ * MAIN CLASS – UseCase9PalindromeCheckerApp
+ * =========================================================
+ *
+ * Use Case 9: Recursive Palindrome Checker
+ *
+ * Description:
+ * This class validates a palindrome using recursion.
+ * It compares the first and last characters recursively
+ * until the base condition is reached.
+ *
+ * Concepts Used:
+ * - Recursion
+ * - Base condition
+ * - Call Stack
+ *
+ * @author Developer
+ * @version 9.0
+ */
 
-class UseCase8PalindromeCheckerApp$Node {
-    char data;
-    UseCase8PalindromeCheckerApp$Node next;
+public class Main {
 
-    UseCase8PalindromeCheckerApp$Node(char var1) {
-        this.data = var1;
-        this.next = null;
+    // Recursive method
+    public static boolean isPalindrome(String input, int start, int end) {
+
+        // Base condition: If pointers cross or meet
+        if (start >= end) {
+            return true;
+        }
+
+        // If characters don’t match
+        if (input.charAt(start) != input.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call
+        return isPalindrome(input, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
     }
 }
