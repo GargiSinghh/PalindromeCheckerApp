1342f38d6c6ac383ec7f6a0dd5bd07ec7182f930
GargiSinghh <gs6295@srmist.edu.in>
2026-03-11 15:05:05 +0530

    UC4:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,26 @@
-/**
- * =========================================================
- * MAIN CLASS – UseCase3PalindromeCheckerApp
- * =========================================================
- *
- * Use Case 3: Palindrome Check Using String Reverse
- *
- * Description:
- * This class reverses a string using a loop and checks
- * whether it is a palindrome.
- *
- * Concepts Used:
- * - For Loop
- * - String Immutability
- * - String Concatenation
- * - equals() Method
- *
- * @author Developer
- * @version 3.0
- */
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
 
-public class Main
-{
+public class UseCase4PalindromeCheckerApp {
+    public static void main(String[] var0) {
+        String var1 = "radar";
+        char[] var2 = var1.toCharArray();
+        int var3 = 0;
+        int var4 = var2.length - 1;
 
-    public static void main(String[] args) {
+        boolean var5;
+        for(var5 = true; var3 < var4; --var4) {
+            if (var2[var3] != var2[var4]) {
+                var5 = false;
+                break;
+            }
 
-        // Original String
-        String input = "madam";
-        String reversed = "";
-
-        // Reverse string using loop
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
+            ++var3;
         }
 
-        // Display original and reversed
-        System.out.println("Original String : " + input);
-        System.out.println("Reversed String : " + reversed);
-
-        // Compare using equals()
-        if (input.equals(reversed)) {
-            System.out.println("Result : It is a Palindrome");
-        } else {
-            System.out.println("Result : It is NOT a Palindrome");
-        }
+        System.out.println("Input : " + var1);
+        System.out.println("Is Palindrome? : " + var5);
     }
 }
